5047aae1fc9d90d20c569de431ea32a17e42f875
github-action-benchmark <github@users.noreply.github.com>
2026-03-02 00:24:12 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 9ed07dc5edf7d6a94ed1c756fdecb724973454e7

--- dev/bench/rust/data.js
+++ dev/bench/rust/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772410117898,
+  "lastUpdate": 1772411052134,
   "repoUrl": "https://github.com/volchanskyi/opengate",
   "entries": {
     "Benchmark": [
@@ -93,6 +93,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "frame_encode_ping",
             "value": 21.199907551150776,
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]",
+            "email": "github-actions[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]",
+            "email": "github-actions[bot]@users.noreply.github.com"
+          },
+          "id": "9ed07dc5edf7d6a94ed1c756fdecb724973454e7",
+          "message": "ci: auto-merge dev → main",
+          "timestamp": "2026-03-02T00:22:44Z",
+          "url": "https://github.com/volchanskyi/opengate/commit/9ed07dc5edf7d6a94ed1c756fdecb724973454e7"
+        },
+        "date": 1772411051703,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "decode_server_hello",
+            "value": 95.31315574561924,
+            "unit": "ns/iter"
+          },
+          {
+            "name": "encode_server_hello",
+            "value": 83.9702990472612,
+            "unit": "ns/iter"
+          },
+          {
+            "name": "frame_decode_control",
+            "value": 661.8352695328492,
+            "unit": "ns/iter"
+          },
+          {
+            "name": "frame_encode_control",
+            "value": 251.65566839190328,
+            "unit": "ns/iter"
+          },
+          {
+            "name": "frame_encode_ping",
+            "value": 21.11355971045045,
             "unit": "ns/iter"
           }
         ]
