6a91c6af9e46dbc973eef93febc4aafe8c1a7b38
Ivan Volchanskyi <ivan.volchanskyi@gmail.com>
2026-04-10 08:59:55 -0700

    chore: document ESLint 10 incompatibility with react-hooks plugin

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,5 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
+// NOTE: eslint-plugin-react-hooks 7.x only supports ESLint ≤9.
+// Do NOT upgrade ESLint to v10+ until a stable react-hooks release adds support.
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
